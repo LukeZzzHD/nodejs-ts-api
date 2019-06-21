e62f4340d4f74f56a561fe6bdc30dc476c9e889d
Lukas Schwab <lukas.schwab@tamedia.ch>
2019-06-21 09:16:57 +0200

    commit changes

--- src/controllers/calculatorController.ts
+++ src/controllers/calculatorController.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
 export function add(req: express.Request, res: express.Response) {
-	let num1 = req.params.num1;
-	let num2 = req.params.num2;
+	let num1 = Number(req.params.num1);
+	let num2 = Number(req.params.num2);
 	let result = num1 + num2;
 
 	res.status(200).json({
 		ok: true,
-		message: 'The two numbers were added successfuly.',
 		data: {
 			result,
 		},
@@ -15,13 +14,12 @@ export function add(req: express.Request, res: express.Response) {
 }
 
 export function subtract(req: express.Request, res: express.Response) {
-	let num1 = req.params.num1;
-	let num2 = req.params.num2;
+	let num1 = Number(req.params.num1);
+	let num2 = Number(req.params.num2);
 	let result = num1 - num2;
 
 	res.status(200).json({
 		ok: true,
-		message: 'The two numbers were subtracted successfuly.',
 		data: {
 			result,
 		},
@@ -29,13 +27,12 @@ export function subtract(req: express.Request, res: express.Response) {
 }
 
 export function multiply(req: express.Request, res: express.Response) {
-	let num1 = req.params.num1;
-	let num2 = req.params.num2;
+	let num1 = Number(req.params.num1);
+	let num2 = Number(req.params.num2);
 	let result = num1 * num2;
 
 	res.status(200).json({
 		ok: true,
-		message: 'The two numbers were multiplied successfuly.',
 		data: {
 			result,
 		},
@@ -43,13 +40,12 @@ export function multiply(req: express.Request, res: express.Response) {
 }
 
 export function divide(req: express.Request, res: express.Response) {
-	let num1 = req.params.num1;
-	let num2 = req.params.num2;
+	let num1 = Number(req.params.num1);
+	let num2 = Number(req.params.num2);
 	let result = num1 / num2;
 
 	res.status(200).json({
 		ok: true,
-		message: 'The two numbers were divided successfuly.',
 		data: {
 			result,
 		},
--- src/controllers/infoController.ts
+++ src/controllers/infoController.ts
@@ -3,6 +3,6 @@ import express from 'express';
 export function getInfo(req: express.Request, res: express.Response) {
 	res.status(200).json({
 		ok: true,
-		message: 'You reached the info endpoint. This API is used only for learning purposes',
+		message: 'You reached the info endpoint. This API is used only for learning purposes.',
 	});
 }
